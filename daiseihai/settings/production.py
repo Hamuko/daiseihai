b19866abe201b888aa361463bfb015494f22d619
Hamuko <hamuko@burakku.com>
2018-07-27 23:38:19 +0300

    Add basic settings for production

--- daiseihai/settings/production.py
+++ daiseihai/settings/production.py
@@ -2,6 +2,9 @@ from daiseihai.settings.shared import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bootleg.hamuko.moe']
+
+STATIC_ROOT = '/var/www/bootleg.hamuko.moe/html/static/'
+MEDIA_ROOT = '/var/www/bootleg.hamuko.moe/html/media/'
 
 VIDEO_URL = 'https://hamuko.moe/4cc/'
