8e5530115d408fc7b5ab38c62004049b836b223b
Hamuko <hamuko@burakku.com>
2018-08-17 22:38:51 +0300

    Add Sentry logging

--- daiseihai/settings/production.py
+++ daiseihai/settings/production.py
@@ -4,6 +4,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['bootleg.hamuko.moe']
 
+RAVEN_CONFIG['dsn'] = secrets.RAVEN_DSN
+
 STATIC_ROOT = '/srv/www/bootleg.hamuko.moe/html/static/'
 MEDIA_ROOT = '/srv/www/bootleg.hamuko.moe/html/media/'
 
