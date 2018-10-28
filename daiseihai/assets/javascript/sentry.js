5e0571ca225e0035339f7f489c1c6b1c3f23b396
Hamuko <hamuko@burakku.com>
2018-10-28 15:38:44 +0200

    Ignore trash errors in Sentry JS SDK

--- daiseihai/assets/javascript/sentry.js
+++ daiseihai/assets/javascript/sentry.js
@@ -5,5 +5,10 @@ export default function setupSentry() {
     Sentry.init({
         dsn: 'https://abca44c8d2a64a9b999a6fdc238b680e@sentry.io/1264504',
         release: release,
+        ignoreErrors: [
+            "ResizeObserver loop limit exceeded",
+            "Synchronous XMLHttpRequest on the main thread is deprecated",
+            "Element.createShadowRoot is deprecated",
+        ],
     });
 }
